108814ffa8cfe866c566062077d38c06e43fb5e1
Valentyn Korniienko <25596072+ValentiWorkLearning@users.noreply.github.com>
2025-12-03 13:09:04 +0200

    Added BSP leds

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -94,6 +94,8 @@ int main(void)
 
   /* Initialize leds */
   BSP_LED_Init(LED_GREEN);
+  BSP_LED_Init(LED_GREEN);
+  BSP_LED_Init(LED_GREEN);
 
   /* Initialize USER push-button, will be used to trigger an interrupt each time it's pressed.*/
   BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
